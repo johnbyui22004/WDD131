32ffe247a674eb0a65d3c697679966e3d35b27f1
johnbyui22004 <johnbrocksman@gmail.com>
2025-05-11 00:00:34 -0600

    yer

--- mission/mission.js
+++ mission/mission.js
@@ -10,12 +10,12 @@ function changeTheme() {
     document.body.classList.add("dark");
     document.body.classList.remove("light");
     // Change the logo to the dark mode logo
-    logo.src = "byui-logo_white.png"; // Replace with your dark mode logo path
+    logo.src = "byui-logo_white.png"; 
   } else if (current === "light") {
     // Change the body to light
     document.body.classList.add("light");
     document.body.classList.remove("dark");
     // Change the logo to the light mode logo
-    logo.src = "byui-logo_blue.webp"; // Replace with your light mode logo path
+    logo.src = "byui-logo_blue.webp"; 
   }
 }
